4a1a71cde3fbc30b0f749a0d15a4e2cc1fd17706
Gabriel Castillo <gabrielcvalenzuela21@gmail.com>
2022-12-20 13:14:37 +0100

    feat(#09) dialogo pago

--- src/web/js/carrito.js
+++ src/web/js/carrito.js
@@ -7,19 +7,30 @@ class Carrito {
 
 		let botonCarrito = document.getElementById("btnCarrito");
 		let btnLogin = document.getElementById("btnLogin");
+
+		function quitarCarrito() {
+			btnLogin.disabled = false;
+			modalCarrito.style.marginTop = "-1000px";
+			modalCarrito.style.visibility = "hidden";
+		}
+
 		botonCarrito.addEventListener("click", () => {
 			btnLogin.disabled = true;
-			modalCarrito.style.display = "block";
+			// modalCarrito.style.display = "block";
 			modalCarrito.style.top = "120px";
 			modalCarrito.style.marginTop = "0px";
 			modalCarrito.style.visibility = "visible";
 		});
-		let cerrar = document.getElementById("cerrarCarrito");
-		cerrar.onclick = () => {
-			btnLogin.disabled = false;
 
-			modalCarrito.style.marginTop = "-1000px";
-			modalCarrito.style.visibility = "hidden";
+		let cerrar = document.getElementById("cerrarCarrito");
+		cerrar.onclick = quitarCarrito;
+
+		let btnPagarCarrito = document.getElementById("pagarCarrito");
+		btnPagarCarrito.onclick = () => {
+			quitarCarrito();
+			let pago = new Pago();
+			console.log(pago);
+			pago.abrirPago();
 		};
 	}
 }
